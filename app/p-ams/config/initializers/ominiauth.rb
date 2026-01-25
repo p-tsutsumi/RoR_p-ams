056102d3676d162ffb13a96c42c70834d7ff8398
p-tsutsumi <kouta.tsutsumi@g.pascalia.co.jp>
2026-01-25 11:25:31 +0900

    fix: sslの検証が外れていたので、修正した。

--- app/p-ams/config/initializers/ominiauth.rb
+++ app/p-ams/config/initializers/ominiauth.rb
@@ -10,9 +10,9 @@ Rails.application.config.middleware.use OmniAuth::Builder do
       identifier: ENV.fetch("KC_CLIENT_ID"),
       secret: ENV.fetch("KC_CLIENT_SECRET"),
       redirect_uri: ENV.fetch("KC_REDIRECT_URI"),
-      connection_opts: {
-        ssl: { verify: false } # ここでSSL検証を無効化
-      }
+      # connection_opts: {
+      #  ssl: { verify: false } # ここでSSL検証を無効化
+      # }
     }
   }
 end
